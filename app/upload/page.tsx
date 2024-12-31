5bdfc2e8551e493e6d7f0f0469be1756e6561df8
priyankahdp <priyankahdp@gmail.com>
2024-12-31 18:31:33 +0100

    Initial commit

---
+++ app/upload/page.tsx
@@ -0,0 +1,11 @@
+const UploadPage = () => {
+    return (
+      <div>
+        <h1>Upload</h1>
+        <p>This is the upload page.</p>
+      </div>
+    );
+  };
+  
+  export default UploadPage;
+  
